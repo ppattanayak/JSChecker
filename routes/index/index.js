b7a5449810c7e0dae01857c705d32f674df7255b
Piyush Pattanayak <ppatnayak@ebay.com>
2016-10-11 15:34:29 -0700

    Updated Package.json

--- routes/index/index.js
+++ routes/index/index.js
@@ -2,13 +2,9 @@ var express = require('express');
 var router = express.Router();
 var template = require('./marko/template.marko');
 var cache = require('../../src/cache');
-var app = require('../../app');
-var path = require('path');
 
-/* GET home page. */
-// app.set('views', path.join(__dirname, 'marko'));
 router.get('/', function(req, res, next) {
-    
+
   var plugins = cache.get('allActivePlugin');
   var options = {
       title: "JSChecker",
